6a80df9e35a361d0004993cd08972ba35a4a3a5e
Ezgi Bas <basezgi@gmail.com>
2023-09-12 11:24:13 -0400

    implement divide

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,6 @@ console.log(`result_subtract = ${result_subtract}`);
 const result_multiply = calc.multiply(5, 4);
 console.log(`result_multiply = ${result_multiply}`);
 
+const result_divide = calc.divide(25, 4);
+console.log(`result_divide = ${result_divide}`);
+
