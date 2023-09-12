86b79cff0741af1fa378a51b366ce8578e24fa7c
notsheepdog <93456568+notsheepdog@users.noreply.github.com>
2023-09-12 11:23:10 -0400

    implement multiply operation

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,6 @@ console.log(`result_add = ${result_add}`);
 const result_subtract = calc.subtract(6, 2);
 console.log(`result_subtract = ${result_subtract}`);
 
+const result_multiply = calc.multiply(5, 4);
+console.log(`result_multiply = ${result_multiply}`);
+
