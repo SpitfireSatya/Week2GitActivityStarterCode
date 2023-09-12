72fd75b9cc99756ccd82b3bda0c80ae927bcf73b
Bryce Erickson <erickson.br@northeastern.edu>
2023-09-12 15:03:55 -0400

    implement multiply operation

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,5 @@ console.log(`result_add = ${result_add}`);
 const result_sub = calc.subtract(3, 2);
 console.log(`result_sub = ${result_sub}`);
 
+const result_multi = calc.multiply(3, 2);
+console.log(`result_sub = ${result_multi}`);
