fdb99be9b2c87e3ff718c10da058a519e3a1cbfd
Nick Payson <38924251+Nick-Payson@users.noreply.github.com>
2023-09-12 11:22:38 -0400

    add subtract operation

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,5 @@ const calc = new Calculator();
 const result_add = calc.add(2, 3);
 console.log(`result_add = ${result_add}`);
 
+const sub_result = calc.subtract(7, 4);
+console.log(`sub_result = ${sub_result}`);
