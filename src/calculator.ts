eccbde2e22814da36534ef5ed97102d899d31061
jia00012 <jia00012@gmail.com>
2023-09-13 17:38:56 -0400

    adding subtract operation again

--- src/calculator.ts
+++ src/calculator.ts
@@ -2,4 +2,12 @@ export class Calculator {
   public add(x: number, y: number) : number {
     return x + y;
   }
+
+  public sub(x: number, y: number) : number {
+    return x - y;
+  }
+
+  public mult(x: number, y: number) : number {
+    return x * y;
+  }
 };
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
+import { isConstructorDeclaration } from 'typescript';
 import { Calculator } from './calculator';
 
 const calc = new Calculator();
 
 const result_add = calc.add(2, 3);
-console.log(`result_add = ${result_add}`);
+const result_sub = calc.sub(3,1);
+const result_mult = calc.mult(5,5);
+
+console.log(`result_add = ${result_add}\nresult_sub = ${result_sub}\nresult_mult = ${result_mult}`);
 
